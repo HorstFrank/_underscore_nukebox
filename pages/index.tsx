2f34458348c5aa379d95483b6bffba4da0c11575
HorstFrank <boris.baumann@gmail.com>
2021-02-17 15:33:12 +0100

    add all my stuff

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,20 +7,20 @@ import cover from "../assets/jsons/cover.json";
 import songs from "../assets/jsons/music.json";
 
 // console.log(songs);
-const sortedSongs = songs.sort((a, b) => {
-  a = a.title;
-  b = b.title;
-  // return a > b ? 1 : a < b ? -1 : 0;
-  if (a > b) {
-    return 1;
-  } else if (a < b) {
-    return -1;
-  } else if (a === b) {
-    return 0;
-  }
-});
+// const sortedSongs = songs.sort((a, b) => {
+//   a = a.title;
+//   b = b.title;
+//   // return a > b ? 1 : a < b ? -1 : 0;
+//   if (a > b) {
+//     return 1;
+//   } else if (a < b) {
+//     return -1;
+//   } else if (a === b) {
+//     return 0;
+//   }
+// });
 
-const list = sortedSongs.map((e) => {
+const list = songs.map((e) => {
   // console.log(e);
   return (
     <SongListItem
